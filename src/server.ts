35ff31fa1df4ee6196963b2eb826cc42c99fd595
Myllene Mendes <mfrsilva@mapfre.com.br>
2025-06-05 17:37:27 -0300

    test/utilizando o framework express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("hello world")
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response:Response) => {
+    response.send("hello world");
+})
+
+app.listen(3000);
